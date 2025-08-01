1613e049e7e5894cc7aafe9bb5ac9865c3396892
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-01 08:33:37 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/productController.js';
-import { fileCheck } from '../middlewares/fileCheck.js';
+import { fileCheck, updateFileCheck } from '../middlewares/fileCheck.js';
 
 
 const router = express.Router();
 
 router.route('/products').get(getProducts).post(fileCheck, createProduct);
-router.route('/products/:id').get(getProduct).patch(updateProduct).delete(deleteProduct)
+router.route('/products/:id').get(getProduct).patch(updateFileCheck, updateProduct).delete(deleteProduct)
 
 
 export default router;
---
+++ utils/removeImage.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+
+export const removeImage = (imagePath) => {
+  fs.unlinkSync(`./uploads/${imagePath}`);
+}
