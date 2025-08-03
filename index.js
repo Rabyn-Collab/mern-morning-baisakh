3d2de78d0edd6405a498d85041423ca1ac7e00e1
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-03 08:59:57 +0545

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
@@ -26,5 +27,6 @@ app.get('/', (req, res) => {
 
 
 app.use(productRoutes);
+app.use(userRoutes);
 
 
