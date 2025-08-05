0580ec412fa5971c182d5cfd4d5a9201f1bbe70a
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-05 08:10:09 +0545

    changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { blogApi } from "../features/blogs/blogApi";
 
 
 
 export const store = configureStore({
   reducer: {
-    [blogApi.reducerPath]: blogApi.reducer
+
   },
 
   //caching, polling ,invalidation
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
-      blogApi.middleware
+
     ]),
 })
