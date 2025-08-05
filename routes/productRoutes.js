a6f7facee7cc805d9438c53b8a515c064e51baaa
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-05 08:06:36 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/productController.js';
 import { fileCheck, updateFileCheck } from '../middlewares/fileCheck.js';
+import { checkAdmin, checkUser } from '../middlewares/checkUser.js';
 
 
 const router = express.Router();
 
-router.route('/products').get(getProducts).post(fileCheck, createProduct);
-router.route('/products/:id').get(getProduct).patch(updateFileCheck, updateProduct).delete(deleteProduct)
+router.route('/products').get(getProducts).post(checkUser, checkAdmin, fileCheck, createProduct);
+router.route('/products/:id').get(getProduct).patch(checkUser, checkAdmin, updateFileCheck, updateProduct).delete(checkUser, checkAdmin, deleteProduct)
 
 
 export default router;
